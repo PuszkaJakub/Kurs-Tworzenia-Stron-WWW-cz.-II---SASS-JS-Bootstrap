1969e4dc5d052f47ed572b642b7fb8bdfabb08e7
Jakub Puszka <126493564+PuszkaJakub@users.noreply.github.com>
2024-07-15 16:49:20 +0200

    Javascript - Zadanie #1
    Treść zadania #1
    Moi drodzy, przed Wami kolejne zadanie! 😎
    
    Treść:
    
    Skopiuj poniższy kod do pliku JS.
    
    Popraw błędnie nazwane zmienne.
    
    Stwórz zmienne z Twoim imieniem oraz wiekiem.
    
    Za pomocą console.log wypisz zdanie "Cześć, jestem imię i mam xlat."
    
    
    
    Kod:
    
    const fav-color = 'red'
    conts meal = 'schabowy'
    const 1favDRINK = 'pepsi'
    const function = 123

---
+++ zadanie7/rozwiazanie.js
@@ -0,0 +1,8 @@
+const favColor = 'red'
+const meal = 'schabowy'
+const favDrink1 = 'pepsi'
+const number = 123
+
+const myName = 'Jakub'
+const myAge = 2
+console.log(`Cześć, jestem ${myName} i mam ${myAge} lat.`)
